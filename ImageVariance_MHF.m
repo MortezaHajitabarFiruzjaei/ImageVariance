84f21f7b7230613a16e9cb135e2951a53dc082ec
Morteza Hajitabar Firuzjaei <34469119+MortezaHajitabarFiruzjaei@users.noreply.github.com>
2018-01-30 10:57:54 +0330

    Update ImageVariance_MHF.m

--- ImageVariance_MHF.m
+++ ImageVariance_MHF.m
@@ -4,7 +4,7 @@ Morteza Hajitabar Firuzjaei
 close all; 
 clear all;
 clc; I = imread('b.png');
-blockSize = [1500 1500];
+blockSize = [64 64];
 varFilterFunction = @(theBlockStructure) var(double(theBlockStructure.data(:)));
 blockyImagevar = blockproc(I, blockSize, varFilterFunction);
 varianceImage=blockyImagevar(:);
